ec328e9156551564e8f0f0341bd7ffaa09569520
Sriram <sriramk2703@gmail.com>
2025-03-22 17:35:36 -0400

    Update

--- test_suit/test_statementCoverage.py
+++ test_suit/test_statementCoverage.py
@@ -3,29 +3,32 @@ from isTriangle import Triangle
 
 class TestStatementCoverage(unittest.TestCase):
 
-    def test_equilateral(self):
+    # Valid Statements
+    def testEquilateral(self):
         self.assertEqual(Triangle.classify(6, 6, 6), Triangle.Type.EQUILATERAL)
 
-    def test_scalene(self):
+    def testScalene(self):
         self.assertEqual(Triangle.classify(7, 10, 5), Triangle.Type.SCALENE)
 
-    def test_isosceles(self):
+    def testIsosceles(self):
         self.assertEqual(Triangle.classify(6, 6, 4), Triangle.Type.ISOSCELES)
 
-    def test_invalid_zero(self):
+    # Invalid Statements
+    def testInvalidZero(self):
         self.assertEqual(Triangle.classify(0, 5, 5), Triangle.Type.INVALID)
 
-    def test_invalid_negative(self):
+    def testInvalidNegative(self):
         self.assertEqual(Triangle.classify(-3, 4, 5), Triangle.Type.INVALID)
 
-    def test_invalid_triangle_inequality_1(self):
+    def testInvalidInequality1(self):
         self.assertEqual(Triangle.classify(1, 2, 3), Triangle.Type.INVALID)
 
-    def test_invalid_triangle_inequality_2(self):
+    def testInvalidInequality2(self):
         self.assertEqual(Triangle.classify(5, 1, 1), Triangle.Type.INVALID)  # Edge case  
 
-    def test_large_numbers(self):
-        self.assertEqual(Triangle.classify(100000, 100000, 100000), Triangle.Type.EQUILATERAL)  # Stress test
+    # Stress Test
+    def stressTest(self):
+        self.assertEqual(Triangle.classify(100000, 100000, 100000), Triangle.Type.EQUILATERAL)
 
 if __name__ == '__main__':
     unittest.main()
